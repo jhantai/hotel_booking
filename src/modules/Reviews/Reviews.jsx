949cfb0baa8eb75ad089ef82a8fedc59b17a6354
jhantai <tylerbroceva123@gmail.com>
2024-06-30 15:56:00 +0600

    add_reviews-end

--- src/modules/Reviews/Reviews.jsx
+++ src/modules/Reviews/Reviews.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import {REVIEWS_DATA} from '../../constants/reviews'
+import Rating from '@mui/material/Rating';
 import style from './style.module.css'
 const Reviews = () => {
     return (
-        <div>
+        <div className={style.reviews_main}>
 
+         <h1 className={style.tittle_tripma}>
+             Что говорят пользователи
+             <span className={style.tripma_span}>Tripma</span>
+         </h1>
+                <div className={style.reviews_container}>
+            {REVIEWS_DATA.map((item, idx) => {
+                return (
+                    <div  key={idx}>
+
+
+
+                            <div className={style.reviews_info}>
+                                <img className={style.reviews_img} src={item.img} alt="d"/>
+                                <h4 className={style.reviews_item}> {item.name}</h4>
+                                <div><Rating defaultValue={item.stars} precision={0.5} readOnly/></div>
+                                <p className={style.reviews_item}>{item.text}</p>
+
+                            </div>
+
+
+                        </div>
+
+
+
+                )
+
+
+            })}
+                </div>
         </div>
     );
 };
