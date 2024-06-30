2ff6e47cf130cbbc089efd6c3c49c7b1772400d0
jhantai <tylerbroceva123@gmail.com>
2024-06-30 20:11:05 +0600

    add_main_hotels

--- src/constants/hotels.js
+++ src/constants/hotels.js
@@ -1,29 +1,29 @@
-import ambassador1 from '../assets/images/ambassador1.png'
+import ambassador1 from '../assets/images/AMBASSADOR.png'
 import ambassador2 from '../assets/images/ambassador2.png'
 import ambassador3 from '../assets/images/ambassador3.png'
 import ambassador4 from '../assets/images/ambassador4.png'
 import ambassador5 from '../assets/images/ambassador5.png'
-import sheraton1 from '../assets/images/sheraton1.png'
+import sheraton1 from '../assets/images/SHERATON.png'
 import sheraton2 from '../assets/images/sheraton2.png'
 import sheraton3 from '../assets/images/sheraton3.png'
 import sheraton4 from '../assets/images/sheraton4.png'
 import sheraton5 from '../assets/images/sheraton5.png'
-import orion1 from '../assets/images/orion1.png'
+import orion1 from '../assets/images/ORION.png'
 import orion2 from '../assets/images/orion2.png'
 import orion3 from '../assets/images/orion3.png'
 import orion4 from '../assets/images/orion4.png'
 import orion5 from '../assets/images/orion5.png'
-import novotel1 from '../assets/images/novotel1.png'
+import novotel1 from '../assets/images/NOVOTEL.png'
 import novotel2 from '../assets/images/novotel2.png'
 import novotel3 from '../assets/images/novotel3.png'
 import novotel4 from '../assets/images/novotel4.png'
 import novotel5 from '../assets/images/novotel5.png'
-import goldentulp1 from '../assets/images/goldentulp1.png'
+import goldentulp1 from '../assets/images/RAMADA.png'
 import goldentulp2 from '../assets/images/goldentulp2.png'
 import goldentulp3 from '../assets/images/goldentulp3.png'
 import goldentulp4 from '../assets/images/goldentulp4.png'
 import goldentulp5 from '../assets/images/goldentulp5.png'
-import goldendragon1 from '../assets/images/goldendragon1.png'
+import goldendragon1 from '../assets/images/GOLDENDRAGON.png'
 import goldendragon2 from '../assets/images/goldendragon2.png'
 import goldendragon3 from '../assets/images/goldendragon3.png'
 import goldendragon4 from '../assets/images/goldendragon4.png'
@@ -41,7 +41,7 @@ export const HOTELS_DATA = [
         ] ,
         name:'Ambassador Hotel' ,
         price:10079 ,
-        text:'Отель Ambassador Hotel находится в городе Бишкек, Кыргызстан по адресу 720040 Бишкек, Orozbekova street 32 в 1.0 км от центра города. Заезд в номера после 14:00, выезд до 12:00.' ,
+        text:'Отель находится в городе Бишкек, Кыргызстан по адресу 720040 Бишкек, улица Орозбекова 32 в 1.0 км от центра города. Заезд в номера после 14:00, выезд до 12:00.' ,
     } ,
     {
         images:[
@@ -54,7 +54,7 @@ export const HOTELS_DATA = [
         ] ,
         name: 'Sheraton Bishkek' ,
         price:19456 ,
-        text:'Отель Sheraton Bishkek находится в городе Бишкек, Кыргызстан по адресу 720001 Бишкек, 148B Kievskaya Street в 0.5 км от центра города. Заезд в номера после 14:00, выезд до 12:00.'
+        text:'Отель находятся в городе Бишкек, Кыргызстан по адресу 720001 Бишкек, 148B улица Киевская в 0.5 км от центра города. Заезд в номера после 14:00, выезд до 12:00. '
     } ,
     {
         images:[
@@ -67,7 +67,7 @@ export const HOTELS_DATA = [
         ] ,
         name: 'Orion Hotel Bishkek ' ,
         price:19637 ,
-        text:'Отель Orion Bishkek расположен в деловом районе Бишкека. При отеле работает спа- и фитнес-центр Elixir с крытым бассейном, сауной и хаммамом'
+        text:'Отель находятся в городе Бишкек, Кыргызстан по адресу 720040 Бишкек, Эркиндик бульвар 21 в 1.4 км от центра города. Заезд в номера после 14:00, выезд до 12:00. '
     } ,
     {
         images:[
@@ -79,7 +79,7 @@ export const HOTELS_DATA = [
         ] ,
         name: 'Novotel Bishkek City Center ' ,
         price:16332 ,
-        text:'Novotel Bishkek City Center — это отель с 5 звездами, расположенный в городе Бишкек. К услугам гостей терраса, ресторан и бар. В числе прочих удобств — доставка еды и напитков и круглосуточная стойка регистрации, а также бесплатный Wi-Fi на всей территории. Гости могут воспользоваться оздоровительным и спа-центром с крытым бассейном, фитнес-центром и сауной.'
+        text:'Отель находится в городе Бишкек, Кыргызстан по адресу 720020 Бишкек, просп.Манаса 16 в 1.6 км от центра города. Заезд в номера после 13:00, выезд до 12:00'
     } ,
     {
         images:[
@@ -91,7 +91,7 @@ export const HOTELS_DATA = [
         ] ,
         name: 'Ramada by Wyndham Bishkek Centre' ,
         price:12145 ,
-        text:'Отель Ramada by Wyndham Bishkek Centre находится в городе Бишкек, Кыргызстан по адресу 720017 Бишкек, 37 Isanov Street в 0.1 км от центра города. Заезд в номера после 13:00, выезд до 12:00.'
+        text:'Отель находятся в городе Бишкек, Кыргызстан по адресу 720017 Бишкек, 37 улица Исанова в 0.1 км от центра города. Заезд в номера после 13:00, выезд до 12:00.'
     } ,
     {
         images:[
@@ -103,6 +103,6 @@ export const HOTELS_DATA = [
         ] ,
         name: 'Golden Dragon' ,
         price:10763,
-        text:'Отель Golden Dragon находится в городе Бишкек, Кыргызстан по адресу 720005 Бишкек, улица Элебаева, 60 в 2.8 км от центра города. Заезд в номера после 14:00, выезд до 12:00.'
+        text:'Отель находятся в городе Бишкек, Кыргызстан по адресу 720005 Бишкек, улица Элебаева 60 в 2.8 км от центра города. Заезд в номера после 14:00, выезд до 12:00.'
     }
 ]
