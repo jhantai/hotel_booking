1ae9cb9a0463f5ffe360e951779662f022b355f9
jhantai <tylerbroceva123@gmail.com>
2024-07-01 14:04:18 +0600

    add_hotels_2x

--- src/constants/hotels.js
+++ src/constants/hotels.js
@@ -1,44 +1,26 @@
 import ambassador1 from '../assets/images/AMBASSADOR.png'
-import ambassador2 from '../assets/images/ambassador2.png'
-import ambassador3 from '../assets/images/ambassador3.png'
-import ambassador4 from '../assets/images/ambassador4.png'
-import ambassador5 from '../assets/images/ambassador5.png'
 import sheraton1 from '../assets/images/SHERATON.png'
-import sheraton2 from '../assets/images/sheraton2.png'
-import sheraton3 from '../assets/images/sheraton3.png'
-import sheraton4 from '../assets/images/sheraton4.png'
-import sheraton5 from '../assets/images/sheraton5.png'
 import orion1 from '../assets/images/ORION.png'
-import orion2 from '../assets/images/orion2.png'
-import orion3 from '../assets/images/orion3.png'
-import orion4 from '../assets/images/orion4.png'
-import orion5 from '../assets/images/orion5.png'
 import novotel1 from '../assets/images/NOVOTEL.png'
-import novotel2 from '../assets/images/novotel2.png'
-import novotel3 from '../assets/images/novotel3.png'
-import novotel4 from '../assets/images/novotel4.png'
-import novotel5 from '../assets/images/novotel5.png'
 import goldentulp1 from '../assets/images/RAMADA.png'
-import goldentulp2 from '../assets/images/goldentulp2.png'
-import goldentulp3 from '../assets/images/goldentulp3.png'
-import goldentulp4 from '../assets/images/goldentulp4.png'
-import goldentulp5 from '../assets/images/goldentulp5.png'
 import goldendragon1 from '../assets/images/GOLDENDRAGON.png'
-import goldendragon2 from '../assets/images/goldendragon2.png'
-import goldendragon3 from '../assets/images/goldendragon3.png'
-import goldendragon4 from '../assets/images/goldendragon4.png'
-import goldendragon5 from '../assets/images/goldendragon5.png'
+import jannat from '../assets/images/jannat.png'
+import centrum from '../assets/images/centrum.png'
+import evropa from '../assets/images/evropa.png'
+import garden from '../assets/images/garden.png'
+import hayat from '../assets/images/Hayat.png'
+import parkHotel from '../assets/images/parkHotel.png'
+import plaza from '../assets/images/plaza.png'
+import smartHotel from '../assets/images/SmartHotel.png'
+import futuro from '../assets/images/futuro.png'
 export const HOTELS_DATA = [
 
     {
         images:[
             ambassador1 ,
-            ambassador2 ,
-            ambassador3 ,
-            ambassador4 ,
-            ambassador5 ,
 
         ] ,
+    free:true ,
         name:'Ambassador Hotel' ,
         price:10079 ,
         text:'Отель находится в городе Бишкек, Кыргызстан по адресу 720040 Бишкек, улица Орозбекова 32 в 1.0 км от центра города. Заезд в номера после 14:00, выезд до 12:00.' ,
@@ -46,12 +28,9 @@ export const HOTELS_DATA = [
     {
         images:[
             sheraton1 ,
-            sheraton2 ,
-            sheraton3 ,
-            sheraton4 ,
-            sheraton5
 
         ] ,
+            free:false ,
         name: 'Sheraton Bishkek' ,
         price:19456 ,
         text:'Отель находятся в городе Бишкек, Кыргызстан по адресу 720001 Бишкек, 148B улица Киевская в 0.5 км от центра города. Заезд в номера после 14:00, выезд до 12:00. '
@@ -59,12 +38,9 @@ export const HOTELS_DATA = [
     {
         images:[
             orion1 ,
-            orion2 ,
-            orion3 ,
-            orion4 ,
-            orion5
 
         ] ,
+            free:true ,
         name: 'Orion Hotel Bishkek ' ,
         price:19637 ,
         text:'Отель находятся в городе Бишкек, Кыргызстан по адресу 720040 Бишкек, Эркиндик бульвар 21 в 1.4 км от центра города. Заезд в номера после 14:00, выезд до 12:00. '
@@ -72,11 +48,9 @@ export const HOTELS_DATA = [
     {
         images:[
             novotel1,
-            novotel2 ,
-            novotel3,
-            novotel4 ,
-            novotel5
+
         ] ,
+            free:false ,
         name: 'Novotel Bishkek City Center ' ,
         price:16332 ,
         text:'Отель находится в городе Бишкек, Кыргызстан по адресу 720020 Бишкек, просп.Манаса 16 в 1.6 км от центра города. Заезд в номера после 13:00, выезд до 12:00'
@@ -84,11 +58,9 @@ export const HOTELS_DATA = [
     {
         images:[
             goldentulp1,
-            goldentulp2,
-            goldentulp3 ,
-            goldentulp4 ,
-            goldentulp5
+
         ] ,
+            free:false ,
         name: 'Ramada by Wyndham Bishkek Centre' ,
         price:12145 ,
         text:'Отель находятся в городе Бишкек, Кыргызстан по адресу 720017 Бишкек, 37 улица Исанова в 0.1 км от центра города. Заезд в номера после 13:00, выезд до 12:00.'
@@ -96,13 +68,104 @@ export const HOTELS_DATA = [
     {
         images:[
             goldendragon1,
-            goldendragon2,
-            goldendragon3 ,
-            goldendragon4 ,
-            goldendragon5
+
         ] ,
+            free:true ,
         name: 'Golden Dragon' ,
         price:10763,
         text:'Отель находятся в городе Бишкек, Кыргызстан по адресу 720005 Бишкек, улица Элебаева 60 в 2.8 км от центра города. Заезд в номера после 14:00, выезд до 12:00.'
+    },
+    {
+        images:[
+            jannat,
+
+        ] ,
+            free:true ,
+        name: 'Jannat Regency' ,
+        price:12267 ,
+        text:'Отель находится в городе Бишкек, Кыргызстан по адресу 720060 Бишкек, улица Аалы Токомбаева, 21/2 в 5.9 км от центра города. Заезд в номера после 14:00, выезд с 12:00 до 13:00.'
+    },
+    {
+        images:[
+            smartHotel,
+
+        ] ,
+            free:false ,
+        name: 'Smart Hotel Bishkek' ,
+        price:11763,
+        text:'Отель находится в городе Бишкек, Кыргызстан по адресу 720140 Бишкек, Улица Абдрахманова 204 в 2.1 км от центра города. Заезд в номера после 14:00, выезд до 12:00.'
+    },
+    {
+        images:[
+            parkHotel,
+
+        ] ,
+            free:false ,
+        name: 'Парк Отель Бишкек' ,
+        price:13853,
+        text:'Отель находится в городе Бишкек, Кыргызстан по адресу 720040 Бишкек, Улица Орозбекова 87 в 1.7 км от центра города. Заезд в номера после 13:00, выезд до 12:00.'
+    },
+    {
+        images:[
+            plaza,
+
+        ] ,
+            free:true ,
+        name: 'Plaza Hotel Bishkek' ,
+        price:9723,
+        text:'Отель находится в городе Бишкек, Кыргызстан по адресу 720044 Бишкек, улица Тоголок Молдо 52 в 1.2 км от центра города. Заезд в номера после 14:00, выезд до 12:00.'
+    },
+    {
+        images:[
+            evropa,
+
+        ] ,
+            free:true ,
+        name: 'Отель Европа' ,
+        price:10363,
+        text: 'Отель находится в городе Бишкек по адресу 720039 , улица Ибраимова 70 в 1.3 км от центра города . Заезд в номера с 14:00 . Выезд до 12:00.'
+    },
+    {
+        images:[
+            garden,
+
+        ] ,
+            free:false ,
+        name: 'Garden Hotel & SPA' ,
+        price:9136,
+        text:'Отель находится в городе Бишкек, Кыргызстан по адресу 720047 Бишкек, Южная Магистраль пересекает ул. Баха. в 5.1 км от центра города. Заезд в номера с 14:00 до 00:00, выезд с 00:00 до 12:00.'
+    },
+    {
+        images:[
+            hayat,
+
+        ] ,
+            free:false ,
+        name: 'Hyatt Regency Bishkek' ,
+        price:16572,
+        text:'Отель находится в городе Бишкек, Кыргызстан по адресу 720011 Бишкек, улица Абдрахманова 191 в 2.1 км от центра города. Заезд в номера после 14:00, выезд до 12:00.'
+    },
+    {
+        images:[
+            centrum,
+
+        ] ,
+            free:true ,
+        name: 'Bishkek Centrum Hotel' ,
+        price:6182,
+        text:'Отель находится в городе Бишкек, Кыргызстан по адресу 720211 Бишкек, улица Султана Ибраимова 113 в 1.4 км от центра города. Заезд с 14:00 до 00:00. Выезд с 12:00 до 12:30.'
+    } ,
+    {
+        images:[
+            futuro,
+
+        ] ,
+        free:false ,
+        name: 'Futuro Hotel Bishkek' ,
+        price:6505,
+        text:'Отель  находится в городе Бишкек, Кыргызстан по адресу 720000 Бишкек, улица Удмуртская 13 в 5.3 км от центра города. Заезд в номера с 14:00 до 00:00, выезд до 12:00.'
     }
+
+
+
 ]
